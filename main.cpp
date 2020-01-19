8cb054079f8dc87fc0a05a08c43c067745b817c1
UdarGIT829 <viraatudar9@gmail.com>
2020-01-18 19:52:43 -0800

    Changed main to accept user input of strings instead of default comparison strings.

--- main.cpp
+++ main.cpp
@@ -6,11 +6,19 @@ Description: This file is the main program file of the project.
 */
 
 #include <iostream>
+#include <string.h>
 #include "Edits.cpp"
 int main()
 {
+	std::string s1;
+	std::string s2;
+	std::cout << "This programs aligns and compares two strings" << std::endl;
+	std::cout << "\tPlease enter the first string to be compared, then press enter: ";
+	std::cin >> s1;
+	std::cout << "\tPlease enter the second string to be compared, then press enter: ";
+	std::cin >> s2;
 	EditDistance edit;
-	edit.findMatrix("broad", "board");
+	edit.findMatrix(s1, s2);
 
 	return 0;
 }
